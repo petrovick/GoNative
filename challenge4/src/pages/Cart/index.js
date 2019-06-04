720a7a605e72b7a14027a64ac661d12175cb0f2d
Gabriel Petrovick <petrovickg@hotmail.com>
2019-06-04 12:21:38 -0300

    delete from cart && add icons on navigation items

--- challenge4/src/pages/Cart/index.js
+++ challenge4/src/pages/Cart/index.js
@@ -33,13 +33,20 @@ class Cart extends Component {
   }
 }
 function SumSubTotal(cartItems) {
-  const subTotalCart = cartItems.reduce(function(a, b) {
-    return (
-      parseInt(a || 0) +
-      parseInt(b.quantity || 0) * parseFloat(b.product.price || 0)
-    ); //* b.product.price;
-  });
-  return subTotalCart;
+  console.tron.log("function SumSubTotal(cartItems) {");
+  console.tron.log(cartItems);
+  if (cartItems && cartItems.length == 1) {
+    return cartItems[0].quantity * cartItems[0].product.price;
+  } else if (cartItems.lenght > 0) {
+    const subTotalCart = cartItems.reduce(function(a, b) {
+      return (
+        parseInt(a || 0) +
+        parseInt(b.quantity || 0) * parseFloat(b.product.price || 0)
+      ); //* b.product.price;
+    });
+  } else {
+    return 0;
+  }
 }
 const mapStateToProps = state => ({
   cart: state.cart,
