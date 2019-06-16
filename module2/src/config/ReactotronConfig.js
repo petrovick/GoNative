41ac77978b6fb3f048c81072fa1218e7c6db9b44
Petrovick <petrovickg@hotmail.com>
2019-06-16 09:10:30 -0300

    merge

--- module2/src/config/ReactotronConfig.js
+++ module2/src/config/ReactotronConfig.js
@@ -2,7 +2,7 @@ import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
   const tron = Reactotron.configure(
-  { host: '192.168.100.4' }) // controls connection & communication settings
+  { host: '192.168.100.2' }) // controls connection & communication settings
     .useReactNative() // add all built-in react native plugins
     .connect(); // let's connect!
 
